f14623d236ca03e91f4df338976a0eaea3307fe6
yozure420 <hatune08mikumiku31@gmail.com>
2025-12-02 17:09:57 +0900

    generate session file

--- py24/20251202_session/app.py
+++ py24/20251202_session/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request,make_response
+from datetime import timedelta
+
 app = Flask(__name__)
 
 @app.route('/.well-known/appspecific/com.chrome.devtools.json')
@@ -8,6 +10,18 @@ def ignore_chrome_devtools():
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/strat_session')
+def start_session():
+    return redirect(url_for('index'))
+
+@app.route('/check_session')
+def check_session():
+    return redirect(url_for('index'))
+
+@app.route('/end_session')
+def check_session():
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run('0.0.0.0',80,True)
